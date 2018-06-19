e2d38e67fc624cffeac7f50dba45c0f34682ae8a
marc <contact@marccollin.com>
2018-06-19 10:39:37 +0200

    #235 : Erreurs sur Différents rapports

---
+++ Crodip-agent-reports/My Project/AssemblyInfo.vb
@@ -0,0 +1,17 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("Crodip-agent-reports")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Crodip-agent-reports")> 
+<Assembly: AssemblyCopyright("Copyright ©  2018")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("2562fb88-0fd2-4c3c-93ea-643318a5ca17")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
