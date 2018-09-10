735ff7314be5ca0016e2e051f4ab29b1c2dad5f0
marc <contact@marccollin.com>
2018-09-10 10:17:11 +0200

    ...

--- TestCrodip/UnitTest2.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System.Text
-Imports Microsoft.VisualStudio.TestTools.UnitTesting
-
-<TestClass()> Public Class UnitTest2
-
-    <TestMethod()> Public Sub TestMethod1()
-    End Sub
-
-End Class
