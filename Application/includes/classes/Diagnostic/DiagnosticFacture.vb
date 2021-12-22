4bc3e367fdc501d0a26ed5d53790d039bd7e57e6
marc <contact@marccollin.com>
2021-12-22 11:21:30 +0100

    Suppression de DiagnosticFacture*

--- Application/includes/classes/Diagnostic/DiagnosticFacture.vb
+++ Application/includes/classes/Diagnostic/DiagnosticFacture.vb
@@ -1,185 +1,185 @@
-Imports System.Web.Services
-Imports System.Xml.Serialization
-Imports System.Collections.Generic
+'Imports System.Web.Services
+'Imports System.Xml.Serialization
+'Imports System.Collections.Generic
 
-<Serializable(), XmlInclude(GetType(Diagnostic))> _
-Public Class DiagnosticFacture
+'<Serializable(), XmlInclude(GetType(Diagnostic))> _
+'Public Class DiagnosticFacture
 
-    Private _id As String
-    Private _factureReference As String
-    Private _factureDate As String
-    Private _factureTotal As String
-    Private _emetteurOrganisme As String
-    Private _emetteurOrganismeAdresse As String
-    Private _emetteurOrganismeCpVille As String
-    Private _emetteurOrganismeTelFax As String
-    Private _emetteurOrganismeSiren As String
-    Private _emetteurOrganismeTva As String
-    Private _emetteurOrganismeRcs As String
-    Private _recepteurRaisonSociale As String
-    Private _recepteurProprio As String
-    Private _recepteurCpVille As String
-    Private _dateModificationAgent As String
-    Private _dateModificationCrodip As String
-    Private _diagnosticFactureItems As List(Of DiagnosticFactureItem)
-
-
-    Sub New()
-
-    End Sub
-
-    Public Property id() As String
-        Get
-            Return _id
-        End Get
-        Set(ByVal Value As String)
-            _id = Value
-        End Set
-    End Property
-
-    Public Property factureReference() As String
-        Get
-            Return _factureReference
-        End Get
-        Set(ByVal Value As String)
-            _factureReference = Value
-        End Set
-    End Property
-
-    Public Property factureDate() As String
-        Get
-            Return _factureDate
-        End Get
-        Set(ByVal Value As String)
-            _factureDate = Value
-        End Set
-    End Property
-
-    Public Property factureTotal() As String
-        Get
-            Return _factureTotal
-        End Get
-        Set(ByVal Value As String)
-            _factureTotal = Value
-        End Set
-    End Property
-
-    Public Property emetteurOrganisme() As String
-        Get
-            Return _emetteurOrganisme
-        End Get
-        Set(ByVal Value As String)
-            _emetteurOrganisme = Value
-        End Set
-    End Property
-
-    Public Property emetteurOrganismeAdresse() As String
-        Get
-            Return _emetteurOrganismeAdresse
-        End Get
-        Set(ByVal Value As String)
-            _emetteurOrganismeAdresse = Value
-        End Set
-    End Property
-
-    Public Property emetteurOrganismeCpVille() As String
-        Get
-            Return _emetteurOrganismeCpVille
-        End Get
-        Set(ByVal Value As String)
-            _emetteurOrganismeCpVille = Value
-        End Set
-    End Property
-
-    Public Property emetteurOrganismeTelFax() As String
-        Get
-            Return _emetteurOrganismeTelFax
-        End Get
-        Set(ByVal Value As String)
-            _emetteurOrganismeTelFax = Value
-        End Set
-    End Property
-
-    Public Property emetteurOrganismeSiren() As String
-        Get
-            Return _emetteurOrganismeSiren
-        End Get
-        Set(ByVal Value As String)
-            _emetteurOrganismeSiren = Value
-        End Set
-    End Property
-
-    Public Property emetteurOrganismeTva() As String
-        Get
-            Return _emetteurOrganismeTva
-        End Get
-        Set(ByVal Value As String)
-            _emetteurOrganismeTva = Value
-        End Set
-    End Property
-
-    Public Property emetteurOrganismeRcs() As String
-        Get
-            Return _emetteurOrganismeRcs
-        End Get
-        Set(ByVal Value As String)
-            _emetteurOrganismeRcs = Value
-        End Set
-    End Property
-
-    Public Property recepteurRaisonSociale() As String
-        Get
-            Return _recepteurRaisonSociale
-        End Get
-        Set(ByVal Value As String)
-            _recepteurRaisonSociale = Value
-        End Set
-    End Property
-
-    Public Property recepteurProprio() As String
-        Get
-            Return _recepteurProprio
-        End Get
-        Set(ByVal Value As String)
-            _recepteurProprio = Value
-        End Set
-    End Property
-
-    Public Property recepteurCpVille() As String
-        Get
-            Return _recepteurCpVille
-        End Get
-        Set(ByVal Value As String)
-            _recepteurCpVille = Value
-        End Set
-    End Property
-
-    Public Property dateModificationAgent() As String
-        Get
-            Return _dateModificationAgent
-        End Get
-        Set(ByVal Value As String)
-            _dateModificationAgent = Value
-        End Set
-    End Property
-
-    Public Property dateModificationCrodip() As String
-        Get
-            Return _dateModificationCrodip
-        End Get
-        Set(ByVal Value As String)
-            _dateModificationCrodip = Value
-        End Set
-    End Property
-
-    Public Property diagnosticFactureItems() As List(Of DiagnosticFactureItem)
-        Get
-            Return _diagnosticFactureItems
-        End Get
-        Set(ByVal Value As List(Of DiagnosticFactureItem))
-            _diagnosticFactureItems = Value
-        End Set
-    End Property
+'    Private _id As String
+'    Private _factureReference As String
+'    Private _factureDate As String
+'    Private _factureTotal As String
+'    Private _emetteurOrganisme As String
+'    Private _emetteurOrganismeAdresse As String
+'    Private _emetteurOrganismeCpVille As String
+'    Private _emetteurOrganismeTelFax As String
+'    Private _emetteurOrganismeSiren As String
+'    Private _emetteurOrganismeTva As String
+'    Private _emetteurOrganismeRcs As String
+'    Private _recepteurRaisonSociale As String
+'    Private _recepteurProprio As String
+'    Private _recepteurCpVille As String
+'    Private _dateModificationAgent As String
+'    Private _dateModificationCrodip As String
+'    Private _diagnosticFactureItems As List(Of DiagnosticFactureItem)
 
 
-End Class
+'    Sub New()
+
+'    End Sub
+
+'    Public Property id() As String
+'        Get
+'            Return _id
+'        End Get
+'        Set(ByVal Value As String)
+'            _id = Value
+'        End Set
+'    End Property
+
+'    Public Property factureReference() As String
+'        Get
+'            Return _factureReference
+'        End Get
+'        Set(ByVal Value As String)
+'            _factureReference = Value
+'        End Set
+'    End Property
+
+'    Public Property factureDate() As String
+'        Get
+'            Return _factureDate
+'        End Get
+'        Set(ByVal Value As String)
+'            _factureDate = Value
+'        End Set
+'    End Property
+
+'    Public Property factureTotal() As String
+'        Get
+'            Return _factureTotal
+'        End Get
+'        Set(ByVal Value As String)
+'            _factureTotal = Value
+'        End Set
+'    End Property
+
+'    Public Property emetteurOrganisme() As String
+'        Get
+'            Return _emetteurOrganisme
+'        End Get
+'        Set(ByVal Value As String)
+'            _emetteurOrganisme = Value
+'        End Set
+'    End Property
+
+'    Public Property emetteurOrganismeAdresse() As String
+'        Get
+'            Return _emetteurOrganismeAdresse
+'        End Get
+'        Set(ByVal Value As String)
+'            _emetteurOrganismeAdresse = Value
+'        End Set
+'    End Property
+
+'    Public Property emetteurOrganismeCpVille() As String
+'        Get
+'            Return _emetteurOrganismeCpVille
+'        End Get
+'        Set(ByVal Value As String)
+'            _emetteurOrganismeCpVille = Value
+'        End Set
+'    End Property
+
+'    Public Property emetteurOrganismeTelFax() As String
+'        Get
+'            Return _emetteurOrganismeTelFax
+'        End Get
+'        Set(ByVal Value As String)
+'            _emetteurOrganismeTelFax = Value
+'        End Set
+'    End Property
+
+'    Public Property emetteurOrganismeSiren() As String
+'        Get
+'            Return _emetteurOrganismeSiren
+'        End Get
+'        Set(ByVal Value As String)
+'            _emetteurOrganismeSiren = Value
+'        End Set
+'    End Property
+
+'    Public Property emetteurOrganismeTva() As String
+'        Get
+'            Return _emetteurOrganismeTva
+'        End Get
+'        Set(ByVal Value As String)
+'            _emetteurOrganismeTva = Value
+'        End Set
+'    End Property
+
+'    Public Property emetteurOrganismeRcs() As String
+'        Get
+'            Return _emetteurOrganismeRcs
+'        End Get
+'        Set(ByVal Value As String)
+'            _emetteurOrganismeRcs = Value
+'        End Set
+'    End Property
+
+'    Public Property recepteurRaisonSociale() As String
+'        Get
+'            Return _recepteurRaisonSociale
+'        End Get
+'        Set(ByVal Value As String)
+'            _recepteurRaisonSociale = Value
+'        End Set
+'    End Property
+
+'    Public Property recepteurProprio() As String
+'        Get
+'            Return _recepteurProprio
+'        End Get
+'        Set(ByVal Value As String)
+'            _recepteurProprio = Value
+'        End Set
+'    End Property
+
+'    Public Property recepteurCpVille() As String
+'        Get
+'            Return _recepteurCpVille
+'        End Get
+'        Set(ByVal Value As String)
+'            _recepteurCpVille = Value
+'        End Set
+'    End Property
+
+'    Public Property dateModificationAgent() As String
+'        Get
+'            Return _dateModificationAgent
+'        End Get
+'        Set(ByVal Value As String)
+'            _dateModificationAgent = Value
+'        End Set
+'    End Property
+
+'    Public Property dateModificationCrodip() As String
+'        Get
+'            Return _dateModificationCrodip
+'        End Get
+'        Set(ByVal Value As String)
+'            _dateModificationCrodip = Value
+'        End Set
+'    End Property
+
+'    Public Property diagnosticFactureItems() As List(Of DiagnosticFactureItem)
+'        Get
+'            Return _diagnosticFactureItems
+'        End Get
+'        Set(ByVal Value As List(Of DiagnosticFactureItem))
+'            _diagnosticFactureItems = Value
+'        End Set
+'    End Property
+
+
+'End Class
--- Application/includes/classes/Diagnostic/DiagnosticFactureItem.vb
+++ Application/includes/classes/Diagnostic/DiagnosticFactureItem.vb
@@ -1,160 +1,160 @@
-Imports System.Web.Services
-Imports System.Xml.Serialization
+'Imports System.Web.Services
+'Imports System.Xml.Serialization
 
-Public Class DiagnosticFactureItem
+'Public Class DiagnosticFactureItem
 
-    Private _id As Integer
-    Private _idFacture As String
-    Private _libelle As String
-    Private _prixUnitaire As String
-    Private _qte As String
-    Private _tva As String
-    Private _prixTotal As String
-    Private _dateModificationAgent As String
-    Private _dateModificationCrodip As String
+'    Private _id As Integer
+'    Private _idFacture As String
+'    Private _libelle As String
+'    Private _prixUnitaire As String
+'    Private _qte As String
+'    Private _tva As String
+'    Private _prixTotal As String
+'    Private _dateModificationAgent As String
+'    Private _dateModificationCrodip As String
 
-    '############################################################################
+'    '############################################################################
 
-    Sub New()
+'    Sub New()
 
-    End Sub
+'    End Sub
 
-    Public Property id() As Integer
-        Get
-            Return _id
-        End Get
-        Set(ByVal Value As Integer)
-            _id = Value
-        End Set
-    End Property
+'    Public Property id() As Integer
+'        Get
+'            Return _id
+'        End Get
+'        Set(ByVal Value As Integer)
+'            _id = Value
+'        End Set
+'    End Property
 
-    Public Property idFacture() As String
-        Get
-            Return _idFacture
-        End Get
-        Set(ByVal Value As String)
-            _idFacture = Value
-        End Set
-    End Property
+'    Public Property idFacture() As String
+'        Get
+'            Return _idFacture
+'        End Get
+'        Set(ByVal Value As String)
+'            _idFacture = Value
+'        End Set
+'    End Property
 
-    Public Property libelle() As String
-        Get
-            Return _libelle
-        End Get
-        Set(ByVal Value As String)
-            _libelle = Value
-        End Set
-    End Property
+'    Public Property libelle() As String
+'        Get
+'            Return _libelle
+'        End Get
+'        Set(ByVal Value As String)
+'            _libelle = Value
+'        End Set
+'    End Property
 
-    Public Property prixUnitaire() As String
-        Get
-            Return _prixUnitaire
-        End Get
-        Set(ByVal Value As String)
-            Try
-                Dim strValue As String = Value
-                strValue = strValue.Replace(".", Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
-                strValue = strValue.Replace(",", Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
-                Dim nValue As Decimal
-                nValue = Convert.ToDecimal(strValue)
-                _prixUnitaire = String.Format("{0:0.00}", nValue)
-                calcultTotal()
-            Catch ex As Exception
+'    Public Property prixUnitaire() As String
+'        Get
+'            Return _prixUnitaire
+'        End Get
+'        Set(ByVal Value As String)
+'            Try
+'                Dim strValue As String = Value
+'                strValue = strValue.Replace(".", Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
+'                strValue = strValue.Replace(",", Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
+'                Dim nValue As Decimal
+'                nValue = Convert.ToDecimal(strValue)
+'                _prixUnitaire = String.Format("{0:0.00}", nValue)
+'                calcultTotal()
+'            Catch ex As Exception
 
-            End Try
-        End Set
-    End Property
+'            End Try
+'        End Set
+'    End Property
 
-    Public Property qte() As String
-        Get
-            Return _qte
-        End Get
-        Set(ByVal Value As String)
-            Try
-                Dim strValue As String = Value
-                strValue = strValue.Replace(".", Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
-                strValue = strValue.Replace(",", Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
-                Dim nValue As Decimal
-                nValue = Convert.ToDecimal(strValue)
-                _qte = strValue
-                calcultTotal()
+'    Public Property qte() As String
+'        Get
+'            Return _qte
+'        End Get
+'        Set(ByVal Value As String)
+'            Try
+'                Dim strValue As String = Value
+'                strValue = strValue.Replace(".", Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
+'                strValue = strValue.Replace(",", Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
+'                Dim nValue As Decimal
+'                nValue = Convert.ToDecimal(strValue)
+'                _qte = strValue
+'                calcultTotal()
 
-            Catch ex As Exception
+'            Catch ex As Exception
 
-            End Try
-        End Set
-    End Property
+'            End Try
+'        End Set
+'    End Property
 
-    Public Property tva() As String
-        Get
-            Return _tva
-        End Get
-        Set(ByVal Value As String)
-            Try
-                Dim strValue As String = Value
-                strValue = strValue.Replace(".", Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
-                strValue = Value.Replace(",", Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
-                Dim nValue As Decimal
-                nValue = Convert.ToDecimal(strValue)
-                _tva = strValue
-                calcultTotal()
+'    Public Property tva() As String
+'        Get
+'            Return _tva
+'        End Get
+'        Set(ByVal Value As String)
+'            Try
+'                Dim strValue As String = Value
+'                strValue = strValue.Replace(".", Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
+'                strValue = Value.Replace(",", Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
+'                Dim nValue As Decimal
+'                nValue = Convert.ToDecimal(strValue)
+'                _tva = strValue
+'                calcultTotal()
 
-            Catch ex As Exception
+'            Catch ex As Exception
 
-            End Try
-        End Set
-    End Property
+'            End Try
+'        End Set
+'    End Property
 
-    Public Property prixTotal() As String
-        Get
-            Return _prixTotal
-        End Get
-        Set(ByVal Value As String)
-            Try
+'    Public Property prixTotal() As String
+'        Get
+'            Return _prixTotal
+'        End Get
+'        Set(ByVal Value As String)
+'            Try
 
-                Dim strValue As String = Value
-                strValue = strValue.Replace(".", Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
-                strValue = Value.Replace(",", Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
-                Dim nValue As Decimal
-                nValue = Convert.ToDecimal(strValue)
-                _prixTotal = String.Format("{0:0.00}", nValue)
-            Catch ex As Exception
+'                Dim strValue As String = Value
+'                strValue = strValue.Replace(".", Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
+'                strValue = Value.Replace(",", Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
+'                Dim nValue As Decimal
+'                nValue = Convert.ToDecimal(strValue)
+'                _prixTotal = String.Format("{0:0.00}", nValue)
+'            Catch ex As Exception
 
-            End Try
-        End Set
-    End Property
+'            End Try
+'        End Set
+'    End Property
 
-    Public Property dateModificationAgent() As String
-        Get
-            Return _dateModificationAgent
-        End Get
-        Set(ByVal Value As String)
-            _dateModificationAgent = Value
-        End Set
-    End Property
+'    Public Property dateModificationAgent() As String
+'        Get
+'            Return _dateModificationAgent
+'        End Get
+'        Set(ByVal Value As String)
+'            _dateModificationAgent = Value
+'        End Set
+'    End Property
 
-    Public Property dateModificationCrodip() As String
-        Get
-            Return _dateModificationCrodip
-        End Get
-        Set(ByVal Value As String)
-            _dateModificationCrodip = Value
-        End Set
-    End Property
+'    Public Property dateModificationCrodip() As String
+'        Get
+'            Return _dateModificationCrodip
+'        End Get
+'        Set(ByVal Value As String)
+'            _dateModificationCrodip = Value
+'        End Set
+'    End Property
 
 
-    Private Sub calcultTotal()
-        Try
-            Dim nqte As Decimal = Convert.ToDecimal(qte)
-            Dim nPU As Decimal = Convert.ToDecimal(prixUnitaire)
-            Dim ntva As Decimal = Convert.ToDecimal(tva)
-            Dim nPrixtotal As Decimal
+'    Private Sub calcultTotal()
+'        Try
+'            Dim nqte As Decimal = Convert.ToDecimal(qte)
+'            Dim nPU As Decimal = Convert.ToDecimal(prixUnitaire)
+'            Dim ntva As Decimal = Convert.ToDecimal(tva)
+'            Dim nPrixtotal As Decimal
 
-            nPrixtotal = nqte * nPU * (1 + (ntva / 100))
-            prixTotal = String.Format("{0:0.00}", nPrixtotal)
+'            nPrixtotal = nqte * nPU * (1 + (ntva / 100))
+'            prixTotal = String.Format("{0:0.00}", nPrixtotal)
 
-        Catch ex As Exception
+'        Catch ex As Exception
 
-        End Try
-    End Sub
-End Class
+'        End Try
+'    End Sub
+'End Class
