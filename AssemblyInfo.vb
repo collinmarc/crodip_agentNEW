48f2df8405a9322c5132425eabd04309c959be8c
marc <contact@marccollin.com>
2022-03-16 14:21:25 +0100

    Build 2022031612000

--- AssemblyInfo.vb
+++ AssemblyInfo.vb
@@ -33,5 +33,5 @@ Imports System.Runtime.InteropServices
 
 <Assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestCrodip")>
 <Assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestCrodip3")>
-<Assembly: AssemblyFileVersionAttribute("2022.03.15.1200")>
+<Assembly: AssemblyFileVersionAttribute("2022.03.16.1200")>
 <Assembly: ComVisible(False)>
