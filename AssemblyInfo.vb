3d54c63b187c6bf20dabc1de37819dd7be9b84ee
marc <contact@marccollin.com>
2025-11-03 17:47:27 +0100

    Relivraison des fichiers d'état

--- AssemblyInfo.vb
+++ AssemblyInfo.vb
@@ -34,5 +34,5 @@ Imports System.Runtime.InteropServices
 <Assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestCrodip")>
 <Assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestCrodip3")>
 <Assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestCrodip4")>
-<Assembly: AssemblyFileVersion("2025.11.03.1000")>
+<Assembly: AssemblyFileVersion("2025.11.03.1500")>
 <Assembly: ComVisible(False)>
